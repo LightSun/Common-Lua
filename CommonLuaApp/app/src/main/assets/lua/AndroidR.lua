a3aaf5543ffea6159aa53c662fe4ef2421d63870
LightSun <donshine723@gmail.com>
2020-02-13 12:14:38 +0800

    opt

--- CommonLuaApp/app/src/main/assets/lua/AndroidR.lua
+++ CommonLuaApp/app/src/main/assets/lua/AndroidR.lua
@@ -12,19 +12,29 @@ function m.ref(str)
     -- 2, push. sub class.
     -- 3. getField
     local classname;
+    local nextIndex;
     if lib.isUpper(arr[1]) then
         classname = andEnv.getClassName(arr[1])
+        nextIndex = 2;
     else
-        classname = arr[1];--todo should fix
+        local name = arr[1];
+        for i = 2, #arr
+        do
+            name = name..'.'..arr[i];
+            if lib.isUpper(arr[i]) then
+                classname = name;
+                nextIndex = i + 1;
+                break
+            end
+        end
     end
 
-    for i = 2, #arr - 1, 1
-    do
-        classname = classname..'$'..arr[i]
+    if(nextIndex == #arr) then
+        error("for refer android R. the name must not only be class name.")
     end
     local tab = andEnv.pushClass(classname);
     return tab.getField(tab, arr[#arr])
 end
 
-print("R.layout.main = ", m.ref('R.layout.main'))
+--print("R.layout.main = ", m.ref('R.layout.main'))
 return m;
